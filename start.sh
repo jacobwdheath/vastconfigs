10b83f4cb754a55a63f4b17d79b3ac8d97c2d3ff
Jacob <39742552+jacobwdheath@users.noreply.github.com>
2022-06-09 06:39:42 +0100

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-apt update -y
-apt-get install -y zip
 apt-get install -y git
 git clone https://github.com/jacobwdheath/vastconfigs.git
 cd vastconfigs
@@ -10,7 +8,7 @@ cd datasets
 wget http://cs231n.stanford.edu/tiny-imagenet-200.zip
 unzip tiny-imagenet-200.zip
 rm tiny-imagenet-200.zip
-cd vastconfigs
+cd ..
 pip install -r requirements.txt
 
 
