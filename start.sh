67995cbbb07985ec8fd5ca134c091b73f9752d36
Jacob <39742552+jacobwdheath@users.noreply.github.com>
2022-06-09 06:27:32 +0100

    Add files via upload

--- start.sh
+++ start.sh
@@ -3,12 +3,14 @@ apt update -y
 apt-get install -y zip
 apt-get install -y git
 git clone https://github.com/jacobwdheath/vastconfigs.git
+cd vastconfigs
 mkdir model-data
 mkdir datasets
 cd datasets
 wget http://cs231n.stanford.edu/tiny-imagenet-200.zip
 unzip tiny-imagenet-200.zip
 rm tiny-imagenet-200.zip
-pip install matplotlib
+cd vastconfigs
+pip install -r requirements.txt
 
 
